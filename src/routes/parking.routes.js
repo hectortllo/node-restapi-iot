a779c5eca09d73a24a151cf921e93159f8a31c82
Héctor Tello <hectortllo@gmail.com>
2020-09-01 19:13:11 -0600

    CRUD básico finalizado

---
+++ src/routes/parking.routes.js
@@ -0,0 +1,54 @@
+import { Router } from "express";
+const router = Router();
+
+// Database connection
+import { connect } from "../database";
+import { ObjectID } from 'mongodb';
+
+router.get('/', async (req, res) => {
+  const db = await connect();
+  const result = await db.collection('parking').find({}).toArray();
+  res.json(result);
+});
+
+router.post('/', async (req, res) => {
+  const db = await connect();
+  const positionParking = {
+    position: req.body.position,
+    taken: false
+  };
+  const result = await db.collection('parking').insertOne(positionParking);
+  res.json(result.ops[0]);
+});
+
+router.get('/:id', async(req, res) => {
+  const { id } = req.params;
+  const db = await connect();
+  const result = await db.collection('parking').findOne({ _id: ObjectID(id) });
+  res.json(result);
+});
+
+router.delete('/:id', async (req, res) => {
+  const { id } = req.params;
+  const db = await connect();
+  const result = await db.collection('parking').deleteOne({ _id: ObjectID(id) });
+  res.json({
+    message: `Parking ${id} deleted`,
+    result
+  });
+});
+
+router.put('/:id', async (req, res) => {
+  const { id } = req.params;
+  const updateParking = {
+    "position": req.body.position,
+    "taken": req.body.taken
+  };
+  const db = await connect();
+  await db.collection('parking').updateOne({ _id: ObjectID(id)}, { $set: updateParking });
+  res.json({
+    message: `Parking spot ${req.body.position} updated`
+  })
+});
+
+export default router;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,18 @@
-import express from 'express';
+import  express, { json } from 'express';
 
 const app = express();
+
+// importing routes
+import IndexRoutes from "./routes/index.routes";
+import ParkingRoutes from "./routes/parking.routes";
 
 // Settings
 app.set('port', process.env.PORT || 3000);
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+// middlewares
+app.use(json());
 
+// routes
+app.use(IndexRoutes);
+app.use('/parking', ParkingRoutes);
 export default app;
